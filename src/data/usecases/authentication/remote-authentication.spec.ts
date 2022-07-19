ac322f1e9a8c2b7dc6415a709aef1d0519f4df69
Luiz Henrique <luizhjramos@outlook.com>
2022-07-18 22:10:38 -0300

    refactor: typing the body response as a generic

--- src/data/usecases/authentication/remote-authentication.spec.ts
+++ src/data/usecases/authentication/remote-authentication.spec.ts
@@ -8,14 +8,19 @@ import { UnexpectedError } from "@/domain/errors/unexpected-error";
 import { RemoteAuthentication } from "./remote-authentication";
 
 import faker from "faker";
+import { AuthenticationParams } from "@/domain/usecases/authentication";
+import { AccountModel } from "@/domain/models/account-model";
 
 type SutTypes = {
   sut: RemoteAuthentication;
-  httpPostClientSpy: HttpPostClientSpy;
+  httpPostClientSpy: HttpPostClientSpy<AuthenticationParams, AccountModel>;
 };
 
 const makeSut = (url: string = faker.internet.url()): SutTypes => {
-  const httpPostClientSpy = new HttpPostClientSpy();
+  const httpPostClientSpy = new HttpPostClientSpy<
+    AuthenticationParams,
+    AccountModel
+  >();
   const sut = new RemoteAuthentication(url, httpPostClientSpy);
 
   return { httpPostClientSpy, sut };
