ff297335a8f3f5f2fb2a37c2a6437a2ed3b98e13
Luiz Henrique <luizhjramos@outlook.com>
2022-11-17 05:53:18 -0300

    refactor: move RequiredFieldValidation instance to a factory test helper method

--- src/validation/required-field/required-field-validation.spec.ts
+++ src/validation/required-field/required-field-validation.spec.ts
@@ -1,20 +1,24 @@
-import faker from 'faker'
+import faker from "faker";
 
 import { RequiredFieldError } from "@/validation/errors";
 import { RequiredFieldValidation } from "@/validation/required-field/required-field-validation";
 
+const factorySetupTestHelper = (): RequiredFieldValidation => {
+  return new RequiredFieldValidation(faker.database.column())
+};
+
 describe("RequiredFieldValidation", () => {
   test("should return error if field is empty", () => {
-    const sut = new RequiredFieldValidation("email");
+    const sut = factorySetupTestHelper();
     const error = sut.validate("");
 
     expect(error).toEqual(new RequiredFieldError());
   });
 
   test("should return false if field isn't empty", () => {
-    const sut = new RequiredFieldValidation("email");
+    const sut = factorySetupTestHelper();
     const error = sut.validate(faker.random.word());
 
-    expect(error).toBeFalsy()
+    expect(error).toBeFalsy();
   });
 });
