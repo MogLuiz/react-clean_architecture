7837a2ab336a673ad58f98f831f6e25f9b22c44e
Luiz Henrique <luizhjramos@outlook.com>
2022-11-17 05:45:46 -0300

    test: ensure RequiredFieldValidation return false if field isn't empty

--- src/validation/required-field/required-field-validation.spec.ts
+++ src/validation/required-field/required-field-validation.spec.ts
@@ -1,3 +1,5 @@
+import faker from 'faker'
+
 import { RequiredFieldError } from "@/validation/errors";
 import { RequiredFieldValidation } from "@/validation/required-field/required-field-validation";
 
@@ -7,5 +9,12 @@ describe("RequiredFieldValidation", () => {
     const error = sut.validate("");
 
     expect(error).toEqual(new RequiredFieldError());
+  });
+
+  test("should return false if field isn't empty", () => {
+    const sut = new RequiredFieldValidation("email");
+    const error = sut.validate(faker.random.word());
+
+    expect(error).toBeFalsy()
   });
 });
