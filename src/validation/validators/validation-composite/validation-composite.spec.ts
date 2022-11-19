3f3d84f7e572952f07ef23f7ef8ebaf433b1dba7
Luiz Henrique <luizhjramos@outlook.com>
2022-11-19 18:34:19 -0300

    refactor: move FieldValidationSpy and ValidationComposite instance to a factory test helper method

--- src/validation/validators/validation-composite/validation-composite.spec.ts
+++ src/validation/validators/validation-composite/validation-composite.spec.ts
@@ -1,18 +1,28 @@
 import { FieldValidationSpy } from "@/validation/validators/test/mock-field-validation";
 import { ValidationComposite } from "@/validation/validators/validation-composite/validation-composite";
 
+type TSetupTestHelper = {
+  setup: ValidationComposite;
+  fieldValidationSpy: FieldValidationSpy[];
+};
+
+const factorySetupTestHelper = (): TSetupTestHelper => {
+  const fieldValidationSpy = [
+    new FieldValidationSpy("any_field"),
+    new FieldValidationSpy("any_field"),
+  ];
+
+  const setup = new ValidationComposite(fieldValidationSpy);
+
+  return { setup, fieldValidationSpy };
+};
+
 describe("ValidationComposite", () => {
   test("should return error if any validation fails", () => {
-    const fieldValidationSpy = new FieldValidationSpy("any_field");
-    const fieldValidationSpy2 = new FieldValidationSpy("any_field");
+    const { fieldValidationSpy, setup } = factorySetupTestHelper();
 
-    fieldValidationSpy.error = new Error("first_error_message");
-    fieldValidationSpy2.error = new Error("second_error_message");
-
-    const setup = new ValidationComposite([
-      fieldValidationSpy,
-      fieldValidationSpy2,
-    ]);
+    fieldValidationSpy[0].error = new Error("first_error_message");
+    fieldValidationSpy[1].error = new Error("second_error_message");
 
     const error = setup.validate("any_field", "any_value");
 
