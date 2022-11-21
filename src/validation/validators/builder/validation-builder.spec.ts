2dbed76e3ee379491e5125c627af1b72d05dbb83
Luiz Henrique <luizhjramos@outlook.com>
2022-11-20 21:16:00 -0300

    test: ensure ValidationBuilder has a email helper

--- src/validation/validators/builder/validation-builder.spec.ts
+++ src/validation/validators/builder/validation-builder.spec.ts
@@ -10,4 +10,9 @@ describe("ValidationBuilder", () => {
     const validations = ValidationBuilder.field("any_field").required().build();
     expect(validations).toEqual([new RequiredFieldValidation("any_field")]);
   });
+
+  test("should return EmailValidation", () => {
+    const validations = ValidationBuilder.field("any_field").email().build();
+    expect(validations).toEqual([new EmailValidation("any_field")]);
+  });
 });
