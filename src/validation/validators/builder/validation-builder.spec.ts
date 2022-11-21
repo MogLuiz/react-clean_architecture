e00e1bb0fcab1c0750c27d6b73af1666c2cfa919
Luiz Henrique <luizhjramos@outlook.com>
2022-11-20 21:13:36 -0300

    refactor: validators index barrel export module

--- src/validation/validators/builder/validation-builder.spec.ts
+++ src/validation/validators/builder/validation-builder.spec.ts
@@ -1,10 +1,13 @@
 import { ValidationBuilder } from "@/validation/validators/builder/validation-builder";
-import { RequiredFieldValidation } from "@/validation/validators/required-field/required-field-validation";
+
+import {
+  EmailValidation,
+  RequiredFieldValidation,
+} from "@/validation/validators";
 
 describe("ValidationBuilder", () => {
   test("should return RequiredFieldValidation", () => {
     const validations = ValidationBuilder.field("any_field").required().build();
-
     expect(validations).toEqual([new RequiredFieldValidation("any_field")]);
   });
 });
