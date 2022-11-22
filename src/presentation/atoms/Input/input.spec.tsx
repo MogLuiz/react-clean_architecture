526316ff12dae8efecf24ebe4d28d562f2fa0c13
Luiz Henrique <luizhjramos@outlook.com>
2022-11-22 05:40:08 -0300

    refactor: move test creation to a factory helper

--- src/presentation/atoms/Input/input.spec.tsx
+++ src/presentation/atoms/Input/input.spec.tsx
@@ -2,11 +2,15 @@ import React from "react";
 import { render, screen } from "@testing-library/react";
 import { Input } from ".";
 
+const factorySetupTestHelper = () => {
+  render(<Input name="field" />);
+  const input = screen.getByTestId("field") as HTMLInputElement;
+  return { input };
+};
+
 describe("Input Atom Component", () => {
   test("should begin with readOnly", () => {
-    render(<Input name="field" />);
-    const input = screen.getByTestId("field") as HTMLInputElement;
-
+    const { input } = factorySetupTestHelper();
     expect(input.readOnly).toBe(true);
   });
 });
