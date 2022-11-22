11928b4e272b57c7d9cade6b0dcaccbca4ff2104
Luiz Henrique <luizhjramos@outlook.com>
2022-11-22 05:45:08 -0300

    test: ensure Input component remove readOnly on focus

--- src/presentation/atoms/Input/input.spec.tsx
+++ src/presentation/atoms/Input/input.spec.tsx
@@ -1,16 +1,26 @@
 import React from "react";
-import { render, screen } from "@testing-library/react";
+import faker from "faker";
+import { fireEvent, render, screen } from "@testing-library/react";
+
 import { Input } from ".";
 
-const factorySetupTestHelper = () => {
-  render(<Input name="field" />);
-  const input = screen.getByTestId("field") as HTMLInputElement;
+const factorySetupTestHelper = (fieldName: string) => {
+  render(<Input name={fieldName} />);
+  const input = screen.getByTestId(fieldName) as HTMLInputElement;
   return { input };
 };
 
 describe("Input Atom Component", () => {
+  const fieldName = faker.database.column();
+
   test("should begin with readOnly", () => {
-    const { input } = factorySetupTestHelper();
+    const { input } = factorySetupTestHelper(fieldName);
     expect(input.readOnly).toBe(true);
+  });
+
+  test("should remove readOnly on focus", () => {
+    const { input } = factorySetupTestHelper(fieldName);
+    fireEvent.focus(input);
+    expect(input.readOnly).toBe(false);
   });
 });
