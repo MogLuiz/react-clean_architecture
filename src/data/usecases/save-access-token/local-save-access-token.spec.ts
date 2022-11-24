d9ed0000f9c59f81efae1486f48100496f3076d6
Luiz Henrique <luizhjramos@outlook.com>
2022-11-24 05:50:36 -0300

    refactor: move setup test creation to a factory helper method

--- src/data/usecases/save-access-token/local-save-access-token.spec.ts
+++ src/data/usecases/save-access-token/local-save-access-token.spec.ts
@@ -3,11 +3,23 @@ import faker from "faker";
 import { SetStorageSpy } from "@/data/test";
 import { LocalSaveAccessToken } from "@/data/usecases/save-access-token/local-save-access-token";
 
+type TFactorySetup = {
+  setup: LocalSaveAccessToken;
+  setStorageSpy: SetStorageSpy;
+};
+
+const factorySetupTestHelper = (): TFactorySetup => {
+  const setStorageSpy = new SetStorageSpy();
+  const setup = new LocalSaveAccessToken(setStorageSpy);
+
+  return { setup, setStorageSpy };
+};
+
 describe("LocalSaveAccessToken", () => {
+  const accessToken = faker.random.uuid();
+
   test("should call SetStorage with correct value", async () => {
-    const setStorageSpy = new SetStorageSpy();
-    const setup = new LocalSaveAccessToken(setStorageSpy);
-    const accessToken = faker.random.uuid();
+    const { setStorageSpy, setup } = factorySetupTestHelper();
 
     await setup.save(accessToken);
 
