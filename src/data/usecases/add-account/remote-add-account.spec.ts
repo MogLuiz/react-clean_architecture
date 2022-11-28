9bafad89b742ffd9daeafaa62e82f9aa0467a9e2
Luiz Henrique <luizhjramos@outlook.com>
2022-11-28 05:27:52 -0300

    test: ensure RemoteAddAccount calls HttpPostClient with correct URL

---
+++ src/data/usecases/add-account/remote-add-account.spec.ts
@@ -0,0 +1,36 @@
+import faker from "faker";
+
+import { HttpPostClientSpy } from "@/data/test";
+import { RemoteAddAccount } from "@/data/usecases/add-account/remote-add-account";
+
+import { AccountModel } from "@/domain/models";
+import { AddAccountParams } from "@/domain/usecases";
+import { mockAddAccountParams } from "@/domain/test";
+
+type TFactorySetup = {
+  setup: RemoteAddAccount;
+  httpPostClientSpy: HttpPostClientSpy<AddAccountParams, AccountModel>;
+};
+
+const factorySetupTestHelper = (
+  url: string = faker.internet.url()
+): TFactorySetup => {
+  const httpPostClientSpy = new HttpPostClientSpy<
+    AddAccountParams,
+    AccountModel
+  >();
+  const setup = new RemoteAddAccount(url, httpPostClientSpy);
+
+  return { httpPostClientSpy, setup };
+};
+
+describe("RemoteAddAccount", () => {
+  test("should call HttpPostclient with correct URL", async () => {
+    const url = faker.internet.url();
+
+    const { httpPostClientSpy, setup } = factorySetupTestHelper(url);
+
+    await setup.add(mockAddAccountParams());
+    expect(httpPostClientSpy.url).toBe(url);
+  });
+});
