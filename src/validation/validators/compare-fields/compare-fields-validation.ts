9ef32941b507a8e6d58a5e5ad38e51d96913f824
Luiz Henrique <luizhjramos@outlook.com>
2022-11-29 05:24:35 -0300

    feat: ensure CompareFieldsValidations throws error if validation is invalid

---
+++ src/validation/validators/compare-fields/compare-fields-validation.ts
@@ -0,0 +1,13 @@
+import { InvalidFieldError } from "@/validation/errors";
+import { IFieldValidation } from "@/validation/protocols/field-validation";
+
+export class CompareFieldsValidation implements IFieldValidation {
+  constructor(
+    readonly field: string,
+    private readonly fieldValueToCompare: string
+  ) {}
+
+  validate(value: string): Error {
+    return new InvalidFieldError();
+  }
+}
