785cfc6cf0095ee8d78db76a2c24fc8fdc40bab5
Luiz Henrique <luizhjramos@outlook.com>
2022-11-29 05:29:23 -0300

    test: ensure CompareFieldsValidations returns falsy if validation succeds

--- src/validation/validators/compare-fields/compare-fields-validation.spec.ts
+++ src/validation/validators/compare-fields/compare-fields-validation.spec.ts
@@ -13,10 +13,19 @@ const factorySetupTestHelper = (
 };
 
 describe("CompareFieldsValidation", () => {
+  const fieldValueToCompare = faker.random.word();
+
   test("should return error if compare is invalid", () => {
-    const setup = factorySetupTestHelper(faker.random.word());
+    const setup = factorySetupTestHelper(fieldValueToCompare);
     const error = setup.validate(faker.random.word());
 
     expect(error).toEqual(new InvalidFieldError());
+  });
+
+  test("should return falsy if compare is valid", () => {
+    const setup = factorySetupTestHelper(fieldValueToCompare);
+    const error = setup.validate(fieldValueToCompare);
+
+    expect(error).toBeFalsy();
   });
 });
